3c25c6f767e33dde4c776f97b3570b74540ca1da
josepotal <josepotal@users.noreply.github.com>
2016-08-25 17:32:31 +0200

    uploading files
    uploading all the files using ipython notebook for the courses related to Python for Everybody Specialization in Coursera

---
+++ Coursera-Python_for_everybody/3. Python to Access Web data/hidden_josep.py
@@ -0,0 +1,4 @@
+def oauth() :
+    return { "consumer_key" : " JtY50m6gc3IgJ0uVYe7CpvEiH", 		"consumer_secret" :"EoskT8XIZ9lMx3XaH2NXbpVa6zU15EHNBRRDTpV0iGUfq9DG0b",
+        "token_key" : "11076417041-39XpMHFHN2XzrWvtsKx5tHIkXf1sGUO01LcVaEV",
+        "token_secret" : "KZUhSRQFqaYt1VrZH0js9XwF8uGt7MvFUv860aUidoaHY" }
